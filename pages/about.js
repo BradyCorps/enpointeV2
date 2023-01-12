824db0dfd71bae7b0c2fc12539dcfb38b94335c3
Brady Corps <bcorps@RWB-MAC-LPT2.local>
2023-01-12 12:16:39 -0600

    EnPointe V2 Begin, pages/routing

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function AboutPage() {
+	return (
+		<div>
+			<h1>About Enpointe.</h1>
+			<p>
+				Enpointe allows you to peer behind the curtain and have a more intimate
+				look at all things Royal Winnipeg Ballet
+			</p>
+			<p>Version 2.00</p>
+		</div>
+	);
+}
---
+++ pages/stories/[slug].js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function StoryPage() {
+	return (
+		<div>
+			<h1>Story List</h1>
+		</div>
+	);
+}
